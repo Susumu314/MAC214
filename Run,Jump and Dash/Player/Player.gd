b5283ef6479a03f390fe27c30b4b7dd96efa0d1c
Hugo Susumu <hugo.asaga@usp.br>
2020-03-08 16:56:41 -0300

    Criação de Fases, Mecânica DashAttack, Obstaculo "BrokenWall"
    Feito mais level design, criação de mecânica que permite dar dash em 4 direcoes e que tambem conseguem quebrar objetos quebraveis, criado obstaculo quebravel, criados sprites necessarios

--- Run,Jump and Dash/Player/Player.gd
+++ Run,Jump and Dash/Player/Player.gd
@@ -12,7 +12,7 @@ var speed = 1500
 var jumpForce = 3000
 var slideSpeed = 600
 var wallJumpLerp = 1200
-var dashSpeed = 2400
+var dashSpeed = 4500
 var power_gem = false
 var canMove = true
 var wallGrab = false
@@ -20,6 +20,7 @@ var wallJumped = false
 var wallSlide = false
 var isDashing = false
 var can_jump = true
+var timer_dash = 0.0
 var leftplataform = 0.0 #usado para permitir que o player pule logo apos sair de uma plataforma
 
 
@@ -30,7 +31,6 @@ func _ready():
 func Walk(dir, delta):
 	if !canMove:
 		return
-	
 	if wallGrab:
 		return
 	
@@ -43,20 +43,15 @@ func jump():
 	if is_on_floor():
 		velocity.y = 5 #para nao ganhar velocidade de queda no solo e para nao bugar o is_on_floor
 	if leftplataform <0.1:
-		if can_jump:
-			if Input.is_action_pressed("jump"):
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -jumpForce
+	elif power_gem:
+			if Input.is_action_just_pressed("jump"):
 				velocity.y = -jumpForce
-				can_jump = false
-	elif power_gem && can_jump:
-			if Input.is_action_pressed("jump"):
-				velocity.y = -jumpForce
-				can_jump = false
 				power_gem = false
-	if !can_jump:
-		if Input.is_action_just_released("jump"):
-			can_jump = true
-			if velocity.y < 0:
-				velocity.y = 0
+	if Input.is_action_just_released("jump"):
+		if velocity.y < 0:
+			velocity.y = 0
 	if is_on_ceiling():
 		velocity.y = +1
 
@@ -72,23 +67,73 @@ func dead():
 func power_gem():
 	power_gem = true
 
+func dash(delta):#personagem dá um dash na direcao que o player esta segurando, priorizando as direcoes verticais e quebra paredes e mata monstros ao contato
+	#if power_gem:
+	if !isDashing:
+		if Input.is_action_just_pressed("dash_attack"):
+			if dir.y >= 0.87:
+				velocity = Vector2(0, 1) * dashSpeed 
+				isDashing = true
+				animations("Dash_r")
+				$Sprite.rotation_degrees = 90
+				return
+			if dir.y <= -0.87:
+				velocity = Vector2(0, -1) * dashSpeed
+				isDashing = true
+				animations("Dash_r")
+				$Sprite.rotation_degrees = 270
+				return
+			if dir.x >= 0.5:
+				velocity = Vector2(1, 0) * dashSpeed
+				isDashing = true
+				animations("Dash_r")
+				return
+			if dir.x <= -0.5:
+				velocity = Vector2(-1, 0) * dashSpeed
+				isDashing = true
+				animations("Dash_r")
+				$Sprite.rotation_degrees = 180
+				return
+	if isDashing:
+		timer_dash += delta
+		if timer_dash > 0.1:
+			isDashing = false
+			timer_dash = 0
+			if velocity.y < 0:
+				velocity = Vector2(0, -jumpForce*0.4)
+			elif velocity.y > 0:
+				pass
+			else:
+				velocity = Vector2(0,0)
+				
+				
 func animations(anim):
 	$Sprite/AnimationPlayer.play(anim)
+	
+	
 func _physics_process(delta):
 	coyote_time(delta)
-	velocity.y = min(velocity.y + GRAVITY, MAX_FALL_SPEED)
 	dir.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
 	dir.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
-	jump()
-	Walk(dir, delta)
+	if !isDashing:
+		$Sprite.rotation_degrees = 0
+		Walk(dir, delta)
+		velocity.y = min(velocity.y + GRAVITY, MAX_FALL_SPEED)
+		jump()
+		if power_gem:
+			animations("Power_Gem_Idle")
+		else:
+			animations("Idle")
+	dash(delta)
+	
+	var collision = move_and_collide(velocity * delta,true, true, true)
+	if collision:
+		if collision.collider.has_method("_break") && isDashing:
+			collision.collider._break()
 	move_and_slide(velocity, UP)
 	for i in get_slide_count():
 		if get_slide_collision(i).collider.name == "Spikes":
 			dead()
-	if power_gem:
-		animations("Power_Gem_Idle")
-	else:
-		animations("Idle")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
