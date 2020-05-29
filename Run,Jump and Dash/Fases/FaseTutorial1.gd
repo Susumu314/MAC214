e8390009b3d1302b16d6559c781851157c45a7a1
HugoSusumuMotaAsaga <hugo.asaga@usp.br>
2020-05-29 16:23:22 -0300

    2h de level desing

--- Run,Jump and Dash/Fases/FaseTutorial1.gd
+++ Run,Jump and Dash/Fases/FaseTutorial1.gd
@@ -9,7 +9,10 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	MusicPlayer.play_music("Level1")
-	pass # Replace with function body.
+	if PlayerInfo.checkpoint == 1:
+		$Player.position = $Checkpoints/Checkpoint1.position	
+	if PlayerInfo.checkpoint == 2:
+		$Player.position = $Checkpoints/Checkpoint2.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
